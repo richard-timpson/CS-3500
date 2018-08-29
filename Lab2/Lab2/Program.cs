4deced455ccf98037be12bd00c6f054e87305a3a
Richard Wallace Timpson <richardtimpson80@gmail.com>
2018-08-29 10:06:45 -0700

    Added a foo function that says hello again.

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -10,7 +10,15 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Lab Class");
+            Console.Read();
+        }
+        /// <summary>
+        /// This is a function that says hello again. 
+        /// </summary>
+        public static void foo()
+        {
+            Console.WriteLine("Saying hello again!");
             Console.Read();
         }
     }
