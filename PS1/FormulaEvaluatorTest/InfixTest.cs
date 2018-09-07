d7f842f4622f856567c655787a0fcb47baa8df1b
Richard Wallace Timpson <richardtimpson3211@hotmail.com>
2018-09-06 18:29:14 -0600

    Got the inital tests working

--- PS1/FormulaEvaluatorTest/InfixTest.cs
+++ PS1/FormulaEvaluatorTest/InfixTest.cs
@@ -23,15 +23,18 @@ namespace FormulaEvaluatorTest
             }
         }
         Evaluator.Lookup lookup = lookupFunction;
-        public static bool ValidTest(string expression, Evaluator.Lookup lookup, int expected)
+        public static bool ValidTest(string expression, Evaluator.Lookup lookup, int expected, out int value)
         {
             Console.WriteLine(expression);
             try
             {
-                return Evaluator.Evaluate(expression, lookup) == expected;
+                value = Evaluator.Evaluate(expression, lookup);
+                return true;
             }
-            catch
+            catch (Exception e)
             {
+                value = 0;
+                Console.WriteLine(e);
                 return false;
             }
 
@@ -92,26 +95,244 @@ namespace FormulaEvaluatorTest
         [TestMethod]
         public void InvalidTestMethod4()
         {
-            string invalidTest = "";
+            string invalidTest = "5 / 0";
             bool invTest = InvalidTest(invalidTest, lookup);
             Assert.IsTrue(invTest);
         }
         /// <summary>
-        /// 
+        /// Two operators in a row
         /// </summary>
         [TestMethod]
         public void InvalidTestMethod5()
         {
-            string invalidTest = "";
+            string invalidTest = " 5 ++ 5";
             bool invTest = InvalidTest(invalidTest, lookup);
             Assert.IsTrue(invTest);
         }
+        /// <summary>
+        /// Two operands in a row
+        /// </summary>
         [TestMethod]
         public void InvalidTestMethod6()
         {
-            string invalidTest = "";
+            string invalidTest = "5 5 + 5";
             bool invTest = InvalidTest(invalidTest, lookup);
             Assert.IsTrue(invTest);
+        }
+        /// <summary>
+        /// less than 2 operands for an operator
+        /// </summary>
+        [TestMethod]
+        public void InvalidTestMethod7()
+        {
+            string invalidTest = "4 +";
+            bool invTest = InvalidTest(invalidTest, lookup);
+            Assert.IsTrue(invTest);
+        }
+        /// <summary>
+        /// Invalid variable name
+        /// </summary>
+        [TestMethod]
+        public void InvalidTestMethod8()
+        {
+            string invalidTest = "a&";
+            bool invTest = InvalidTest(invalidTest, lookup);
+            Assert.IsTrue(invTest);
+        }
+        /// <summary>
+        /// Invalid formula followed by valid formula
+        /// </summary>
+        [TestMethod]
+        public void InvalidTestMethod9()
+        {
+            string invalidTest = "(8+9) -(5/2) * A6 / (5 - 5)";
+            bool invTest = InvalidTest(invalidTest, lookup);
+            Assert.IsTrue(invTest);
+            string validTest = "(2 + 2)";
+            int expected = 4;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Valid Tests
+
+            //Basic operations
+
+
+        /// <summary>
+        /// add 
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod1()
+        {
+            string validTest = "2 + 2";
+            int expected = 4;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///  subtract
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod2()
+        {
+            string validTest = "3 - 1";
+            int expected = 2;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///  Subtract to get negative number
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod3()
+        {
+            string validTest = "1 - 3";
+            int expected = -2;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///  Multiplication 
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod4()
+        {
+            string validTest = "4 * 8";
+            int expected = 32;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///  Division
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod5()
+        {
+            string validTest = "8 / 4";
+            int expected = 2;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///  Fraction division
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod6()
+        {
+            string validTest = "4 / 8";
+            int expected = 0;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        /// OOO
+
+        /// <summary>
+        ///  Multiplication with addition
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod7()
+        {
+            string validTest = "7 * 8 + 4";
+            int expected = 60;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///  Division with addition
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod8()
+        {
+            string validTest = "10/2 + 4";
+            int expected = 9;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///  Addition with subtraction
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod9()
+        {
+            string validTest = "4 - 4 + 10";
+            int expected = 10;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///  testing with parenthesis
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod10()
+        {
+            string validTest = "(7 + 4) * 8";
+            int expected = 88;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///  variable
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod11()
+        {
+            string validTest = "A5 + 2";
+            int expected = 12;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///  divide by 0 in numerator
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod12()
+        {
+            string validTest = "0 / 5";
+            int expected = 0;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        ///  Complicated string with two variables
+        /// </summary>
+        [TestMethod]
+        public void ValidTestMethod13()
+        {
+            string validTest = "(2+1) - (4*5) /(9-7) - A5 / A6 + 5";
+            int expected = -7;
+            int actual = 0;
+            bool valTest = ValidTest(validTest, lookup, expected, out actual);
+            Assert.IsTrue(valTest);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- PS1/PS1-Console App/Program.cs
+++ PS1/PS1-Console App/Program.cs
@@ -13,50 +13,7 @@ namespace PS1_Console_App
         static void Main(string[] args)
         {
             Evaluator.Lookup lookup = lookupFunction; 
-            //invalid tests
-            string invalid_test1 = ""; // only whitespace: Successful
-            string invalid_test2 = "()"; // only parenthesis: Successful
-            string invalid_test3 = "(8 * (4+2))) - 4"; //Invalid parenthesis: 
-            string invalid_test4 = " 5 / 0"; //division by 0 
-            string invalid_test5 = "5 ++5 "; //two operators in a row
-            string invalid_test6 = "5 5 + 5"; //two operands in a row"
-            string invalid_test7 = "4 +"; // <2 operands for an operator
-            string invalid_test8 = "a&"; // invalid variable name
-            string invalid_test9 = "(8+9) -(5/2) * A6 / (5 - 5)"; //invalid formula
-            string invalid_test10 = "(2 + 2)"; // followed by valid formula
-
-            //valid tests
-            
-            //basic operations
-            string valid_test1 = "2 + 2"; // add 4
-            string valid_test2 = "3 -1 "; //subtract 2
-            string valid_test3 = "1 - 3"; //subtract to get negative number -2
-            string valid_test4 = "4 * 8"; //multiplication 32
-            string valid_test5 = "8/4"; //division 2
-            string valid_test6 = "4 / 8"; //fraction .5
-
-            //order of operations
-
-            string valid_test7 = "7 * 8 + 4"; // multiplication with addition
-            string valid_test8 = "10/2 + 4"; // division with addition
-            string valid_test9 = "4 - 4 + 10"; //addition and subtraction
-            string valid_test10 = "(7 + 4) * 8"; //testing with simple parenthesis
-
-
-            string valid_test11 = "(2+1) - (4*5) /(9-7) - A5 / A6 + 5"; // random string
-            string valid_test12 = "0 / 5"; //divide by 0 in numerator
-
-            bool invTest = InvalidTest(invalid_test3, lookup);
-            if (invTest)
-            {
-                Console.WriteLine("Test was invalid");
-            }
-            int expected = 0;
-            bool validTest = ValidTest(valid_test12, lookup, expected);
-            if (validTest)
-            {
-                Console.WriteLine("Test was Valid. Output: " +  expected );
-            }
+ 
 
             Console.Read();
 
@@ -76,36 +33,6 @@ namespace PS1_Console_App
                 throw new ArgumentException("Invalid variable");
             }
         }
-        public static bool ValidTest(string expression, Evaluator.Lookup lookup , int expected) 
-        {
-            Console.WriteLine(expression);
-            try {
-               return Evaluator.Evaluate(expression, lookup) == expected;
-            }
-            catch {
-                return false;
-            }
 
-        }
-        public static bool InvalidTest(string expression, Evaluator.Lookup lookup)
-        {
-            Console.WriteLine(expression);
-            try 
-            {
-                Evaluator.Evaluate(expression, lookup);
-                return false;
-            }
-            catch (ArgumentException e) 
-            {
-                Console.WriteLine(e);
-                return true;
-            }
-            catch(Exception e) 
-            {
-                Console.WriteLine(e);
-                return false;
-            }
-
-        }
     }
 }
