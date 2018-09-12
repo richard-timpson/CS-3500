b46168c5ad7069996ac2db09d2c28f47d790a1dd
Richard Wallace Timpson <richardtimpson3211@hotmail.com>
2018-09-11 18:14:36 -0600

    Got all of the intial functions written. Now need to test.

---
+++ PS2/DevelopmentTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevelopmentTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("College of Engineering")]
+[assembly: AssemblyProduct("DevelopmentTests")]
+[assembly: AssemblyCopyright("Copyright © College of Engineering 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d74cc678-2345-4fc0-9312-9b7e00179c41")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
