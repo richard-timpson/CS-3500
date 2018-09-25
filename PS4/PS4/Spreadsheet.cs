d1d63b5900b4125d67dd76e78e7fc06f8ab7b457
Richard Wallace Timpson <richardtimpson3211@hotmail.com>
2018-09-25 17:17:57 -0600

    Random commit

--- PS4/PS4/Spreadsheet.cs
+++ PS4/PS4/Spreadsheet.cs
@@ -4,9 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SpreadsheetUtilities;
+
 namespace SS
 {
-    class Spreadsheet: AbstractSpreadsheet
+    public class Spreadsheet : AbstractSpreadsheet
     {
+        private Dictionary<string, string> NonemptyCells = new Dictionary<string, string>();
+        public override IEnumerable<String> GetNamesOfAllNonemptyCells()
+        {
+            List<string> NonemptyCellNames = new List<string>();
+
+            foreach (KeyValuePair<string, string> cell in NonemptyCells)
+            {
+                NonemptyCellNames.Add(cell.Key);
+            }
+            return NonemptyCellNames;
+        }
+
+
+        public override object GetCellContents(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override ISet<String> SetCellContents(String name, double number)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override ISet<String> SetCellContents(String name, String text)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override ISet<String> SetCellContents(String name, Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        protected override IEnumerable<String> GetDirectDependents(String name)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
+
+
+
 }
---
+++ PS4/SpreadsheetTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpreadsheetTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("College of Engineering")]
+[assembly: AssemblyProduct("SpreadsheetTests")]
+[assembly: AssemblyCopyright("Copyright © College of Engineering 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("96efe24d-7c0e-494e-b8a8-8adf21535f31")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
