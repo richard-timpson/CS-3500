f40ab685e53c90d06b059ee9bc4d08803a4cfae7
Richard Wallace Timpson <richardtimpson3211@hotmail.com>
2018-11-14 18:29:36 -0700

    HEISENBUG!!! trying to figure out why network controller is not processing messages correctly.

--- SpaceWars/GameController/GameController.cs
+++ SpaceWars/GameController/GameController.cs
@@ -69,6 +69,7 @@ namespace Game
             Console.WriteLine("Calling Receive World Function");
             string totalData = ss.sb.ToString();
             string[] parts = Regex.Split(totalData, @"(?<=[\n])");
+
             foreach (string s in parts)
             {
                 if (s.Length == 0)
@@ -77,18 +78,24 @@ namespace Game
                 }
                 if (s[s.Length - 1] != '\n')
                 {
+                    Console.WriteLine(s);
+                    Console.WriteLine("Incomplete Message");
                     Networking.NetworkController.GetData(ss);
                     break;
+                }
+                if (s[s.Length -1] == '\n')
+                {
+                    s.TrimEnd('\n');
+                }
+                lock (this.theWorld)
+                {
+                    ProcessObject(s);
                 }
                 
             }
             ss.sb.Clear();
-            lock (this.theWorld)
-            {
-                ProcessObject(parts);
-            }
-            WorldUpdated();
             Networking.NetworkController.GetData(ss);
+            WorldUpdated();
 
 
 
@@ -102,75 +109,74 @@ namespace Game
         }
 
 
-        private void ProcessObject(string[] parts)
+        private void ProcessObject(string s )
         {
-            foreach (string s in parts)
+            Ship temp;
+            Star tempStar;
+            Projectile tempProj;
+            Console.WriteLine(s);
+            // if the object is a ship
+            if (s.Length >= 4 && s[2] == 's' && s[3] == 'h')
             {
-                Ship temp;
-                Star tempStar;
-                Projectile tempProj;
-                Console.WriteLine(s);
-                // if the object is a ship
-                if (s.Length >= 4 && s[2] == 's' && s[3] == 'h')
+                temp = JsonConvert.DeserializeObject<Ship>(s);
+                // if the ship isn't in the current list, and it is not dead
+                if (!theWorld.GetShips().Any(item => item.ID == temp.ID && temp.hp != 0))
                 {
-                    temp = JsonConvert.DeserializeObject<Ship>(s);
-                    // if the ship isn't in the current list, and it is not dead
-                    if (!theWorld.GetShips().Any(item => item.ID == temp.ID && temp.hp != 0))
-                    {
-                        theWorld.AddShip(temp);
-                    }
-                    // if the ship is in the current list, and it is not dead
-                    else if (theWorld.GetShips().Any(item => item.ID == temp.ID) && temp.hp != 0)
-                    {
-                        // remove old ship
-                        theWorld.RemoveShip(temp.ID);
-                        // add new ship
-                        theWorld.AddShip(temp);
-                    }
-                    // if the ship is dead, remove it
-                    else if (temp.hp == 0)
-                    {
-                        // need to tell view to explode 
+                    theWorld.AddShip(temp);
+                }
+                // if the ship is in the current list, and it is not dead
+                if (theWorld.GetShips().Any(item => item.ID == temp.ID) && temp.hp != 0)
+                {
+                    Console.WriteLine(temp.hp);
+                    // remove old ship
+                    theWorld.RemoveShip(temp.ID);
+                    // add new ship
+                    theWorld.AddShip(temp);
+                }
+                // if the ship is dead, remove it
+                if (theWorld.GetShips().Any(item => item.ID == temp.ID) && temp.hp == 0)
+                {
+                    // need to tell view to explode 
 
-                        theWorld.RemoveShip(temp.ID);
-                    }
-                }
-                // if the object is a start
-                if (s.Length >= 4 && s[2] == 's' && s[3] == 't')
-                {
-                    tempStar = JsonConvert.DeserializeObject<Star>(s);
-                    // if star does not exist
-                    if (!theWorld.GetStars().Any(item => item.ID == tempStar.ID))
-                    {
-                        // add star
-                        theWorld.AddStar(tempStar);
-                    }
-                }
-                // if the object is a projectile
-                if (s.Length >= 4 && s[2] == 'p')
-                {
-                    tempProj = JsonConvert.DeserializeObject<Projectile>(s);
-                    // if projectile does not exists
-                    if (!theWorld.GetProjectiles().Any(item => item.ID == tempProj.ID))
-                    {
-                        // add projectile
-                        theWorld.AddProjectile(tempProj);
-                    }
-                    // if projectile exists and is alive
-                    else if (theWorld.GetProjectiles().Any(item => item.ID == tempProj.ID) && tempProj.alive)
-                    {
-                        // remove the old projectile
-                        theWorld.RemoveProjectile(tempProj.ID);
-                        // add the new one
-                        theWorld.AddProjectile(tempProj);
-                    }
-                    // if projectile is dead
-                    else if (tempProj.alive == false)
-                    {
-                        theWorld.RemoveProjectile(tempProj.ID);
-                    }
+                    theWorld.RemoveShip(temp.ID);
                 }
             }
+            // if the object is a start
+            if (s.Length >= 4 && s[2] == 's' && s[3] == 't')
+            {
+                tempStar = JsonConvert.DeserializeObject<Star>(s);
+                // if star does not exist
+                if (!theWorld.GetStars().Any(item => item.ID == tempStar.ID))
+                {
+                    // add star
+                    theWorld.AddStar(tempStar);
+                }
+            }
+            // if the object is a projectile
+            if (s.Length >= 4 && s[2] == 'p')
+            {
+                tempProj = JsonConvert.DeserializeObject<Projectile>(s);
+                // if projectile does not exists
+                if (!theWorld.GetProjectiles().Any(item => item.ID == tempProj.ID))
+                {
+                    // add projectile
+                    theWorld.AddProjectile(tempProj);
+                }
+                // if projectile exists and is alive
+                if (theWorld.GetProjectiles().Any(item => item.ID == tempProj.ID) && tempProj.alive)
+                {
+                    // remove the old projectile
+                    theWorld.RemoveProjectile(tempProj.ID);
+                    // add the new one
+                    theWorld.AddProjectile(tempProj);
+                }
+                // if projectile is dead
+                if (tempProj.alive == false)
+                {
+                    theWorld.RemoveProjectile(tempProj.ID);
+                }
+            }
+            
         }
     }
 }
--- SpaceWars/NetworkController/NetworkController.cs
+++ SpaceWars/NetworkController/NetworkController.cs
@@ -24,7 +24,7 @@ namespace NetworkController
 
             public callMe _call;
             // This is the buffer where we will receive data from the socket
-            public byte[] messageBuffer = new byte[400000];
+            public byte[] messageBuffer = new byte[4000];
 
             // This is a larger (growable) buffer, in case a single receive does not contain the full message.
             public StringBuilder sb = new StringBuilder();
@@ -167,7 +167,7 @@ namespace NetworkController
                     // Append the received data to the growable buffer.
                     // It may be an incomplete message, so we need to start building it up piece by piece
                     ss.sb.Append(theMessage);
-
+                    Console.WriteLine("The message in the network controlle: " + theMessage);
                     ss._call(ss);
                 }
 
